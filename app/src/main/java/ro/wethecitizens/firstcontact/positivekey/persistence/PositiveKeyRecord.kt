1add93c53f738009e0d458c45bca1ca4f5d9a841
img-andro-dev <64158678+img-andro-dev@users.noreply.github.com>
2020-04-25 16:21:02 +0300

    Initial

--- app/src/main/java/ro/wethecitizens/firstcontact/positivekey/persistence/PositiveKeyRecord.kt
+++ app/src/main/java/ro/wethecitizens/firstcontact/positivekey/persistence/PositiveKeyRecord.kt
@@ -2,18 +2,17 @@ package ro.wethecitizens.firstcontact.positivekey.persistence
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.Index
 import androidx.room.PrimaryKey
 import java.util.*
 
 @Entity(
-    tableName = "positive_key_table",
-    indices = [
-        Index("key"),
-        Index("key_date")
-    ]
+    tableName = "positive_key_table"
 )
 data class PositiveKeyRecord (
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    var id: Long = 0,
 
     @ColumnInfo(name = "key")
     val key: String,
@@ -21,8 +20,3 @@ data class PositiveKeyRecord (
     @ColumnInfo(name = "key_date")
     val keyDate: Calendar = Calendar.getInstance()
 )
-{
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
-    var id: Long = 0
-}
