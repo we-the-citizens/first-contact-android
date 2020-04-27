c3a82be7b199743b2f75c47f0aec8bbacc432446
George Stancu <sgeorge225@gmail.com>
2020-04-27 21:51:45 +0300

    [ANDROID-13] added alert others fragment

---
+++ app/src/main/java/ro/wethecitizens/firstcontact/alert/AlertOthersFragment.kt
@@ -0,0 +1,6 @@
+package ro.wethecitizens.firstcontact.alert
+
+import androidx.fragment.app.Fragment
+import ro.wethecitizens.firstcontact.R
+
+class AlertOthersFragment : Fragment(R.layout.fragment_alert_others)
