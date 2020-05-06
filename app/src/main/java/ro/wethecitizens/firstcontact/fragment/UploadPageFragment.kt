b2fbfca021a6ce97a069be042d4205ea2aaf6bb1
img-andro-dev <64158678+img-andro-dev@users.noreply.github.com>
2020-05-07 00:05:44 +0300

    ANDROID-27 - First try

--- app/src/main/java/ro/wethecitizens/firstcontact/fragment/UploadPageFragment.kt
+++ app/src/main/java/ro/wethecitizens/firstcontact/fragment/UploadPageFragment.kt
@@ -1,18 +1,30 @@
 package ro.wethecitizens.firstcontact.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_upload_page.*
 import ro.wethecitizens.firstcontact.BuildConfig
+import ro.wethecitizens.firstcontact.MainActivity
 import ro.wethecitizens.firstcontact.R
 import ro.wethecitizens.firstcontact.status.persistence.StatusRecord
 import ro.wethecitizens.firstcontact.streetpass.persistence.StreetPassRecord
 
 data class ExportData(val recordList: List<StreetPassRecord>, val statusList: List<StatusRecord>)
 
-class UploadPageFragment : Fragment(R.layout.fragment_upload_page) {
+class UploadPageFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_upload_page, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -21,6 +33,12 @@ class UploadPageFragment : Fragment(R.layout.fragment_upload_page) {
 
         uuidString = uuidString.substring(uuidString.length - 4)
         fragment_buildno.text = "${BuildConfig.GITHASH}-${uuidString}"
+        val childFragMan: FragmentManager = childFragmentManager
+        val childFragTrans: FragmentTransaction = childFragMan.beginTransaction()
+        val fragB = VerifyCallerFragment()
+        childFragTrans.add(R.id.fragment_placeholder, fragB)
+        childFragTrans.addToBackStack("VerifyCaller")
+        childFragTrans.commit()
     }
 
     fun turnOnLoadingProgress() {
@@ -31,13 +49,31 @@ class UploadPageFragment : Fragment(R.layout.fragment_upload_page) {
         uploadPageFragmentLoadingProgressBarFrame.visibility = View.INVISIBLE
     }
 
+    fun navigateToUploadPin() {
+        val childFragMan: FragmentManager = childFragmentManager
+        val childFragTrans: FragmentTransaction = childFragMan.beginTransaction()
+        val fragB = EnterPinFragment()
+        childFragTrans.add(R.id.fragment_placeholder, fragB)
+        childFragTrans.addToBackStack("C")
+        childFragTrans.commit()
+    }
+
+    fun goBackToHome() {
+        var parentActivity = activity as MainActivity
+        parentActivity.goToHome()
+    }
+
     fun navigateToUploadComplete() {
-        Navigation.findNavController(requireActivity(), R.id.nav_upload_fragment)
-            .navigate(R.id.action_enterPinFragment_to_uploadCompleteFragment)
+        val childFragMan: FragmentManager = childFragmentManager
+        val childFragTrans: FragmentTransaction = childFragMan.beginTransaction()
+        val fragB = UploadCompleteFragment()
+        childFragTrans.add(R.id.fragment_placeholder, fragB)
+        childFragTrans.addToBackStack("UploadComplete")
+        childFragTrans.commit()
     }
 
     fun popStack() {
-        Navigation.findNavController(requireActivity(), R.id.nav_upload_fragment)
-            .popBackStack()
+        val childFragMan: FragmentManager = childFragmentManager
+        childFragMan.popBackStack()
     }
 }
